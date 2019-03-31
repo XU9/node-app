6b0e562e8582e3fb7ff01a2ec00fc231ed574aa7
nhaxyz <nhaxyz@163.com>
2019-03-31 21:32:57 +0800

    login接口

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcrypt");
-const gravatar = require ('gravatar');
+const gravatar = require('gravatar');
 
 router.get("/test", (req, res) => {
     res.json({
@@ -22,14 +22,13 @@ router.post("/register", (req, res) => {
             if (user) {
                 return res.status(400).json({ email: "邮箱已被注册" });
             } else {
-                const avatar = gravatar.url(req.body.email,{s:'200',r:'pg',d:'mm'});
+                const avatar = gravatar.url(req.body.email, { s: '200', r: 'pg', d: 'mm' });
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
                     avatar,
                     password: req.body.password
                 });
-
                 bcrypt.genSalt(10, function (err, salt) {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err;
@@ -44,10 +43,31 @@ router.post("/register", (req, res) => {
                 });
             }
         })
-    // res.json({
-    //     msg: "user register"
-    // })
 })
 
+// $router POST api/user/login
+// @desc 
+// access public
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // 查询数据库  
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: "用户不存在！" });
+            }
 
+            // 密码匹配
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: "success" });
+                    }else{
+                        return res.status(400).json({password:"密码错误"});
+                    }
+                })
+        })
+
+})
 module.exports = router;
