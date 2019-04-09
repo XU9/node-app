45784301e0e244f026f78dd58c038d6fe9ad50bf
nhaxyz <nhaxyz@163.com>
2019-04-09 21:31:29 +0800

    首页个人信息

--- client/src/router.js
+++ client/src/router.js
@@ -4,6 +4,8 @@ import Index from './views/Index.vue'
 import Register from "./views/Register"
 import Login from './views/Login'
 import Nofind from './views/404'
+import Home from './views/Home'
+import InfoShow from './views/InfoShow'
 
 
 Vue.use(Router)
@@ -19,7 +21,12 @@ const router = new Router({
     {
       path: '/index',
       name: 'index',
-      component: Index
+      component: Index,
+      children: [
+        { path: '', component: Home },
+        { path: '/home', name: 'home', component: Home },
+        { path: '/infoshow', name: 'infoshow', component: InfoShow },
+      ]
     },
     {
       path: '/register',
--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,3 +1,33 @@
 <template>
-    <div>this is index</div>
+  <div class="index">
+    <HeadNav></HeadNav>
+     <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import HeadNav from "../components/HeadNav";
+
+export default {
+  name: "index",
+  components: {
+    HeadNav,
+  }
+};
+</script>
+<style scoped>
+.index {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.rightContainer {
+  position: relative;
+  top: 0;
+  left: 180px;
+  width: calc(100% - 180px);
+  height: calc(100% - 71px);
+  overflow: auto;
+}
+</style>
+
